f688aa7c81201534f7a01ffa414493a027d0c447
Damt26 <damttrabajos@gmail.com>
2023-03-20 15:52:56 -0500

    primer commit

---
+++ src/main/java/com/damt/SpringBootProy/SpringBootProyApplication.java
@@ -0,0 +1,13 @@
+package com.damt.SpringBootProy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damt/SpringBootProy/SpringBootProyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damt.SpringBootProy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
